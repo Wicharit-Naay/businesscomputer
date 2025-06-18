0dd4a163cda9f40ffd9e85f6028e7e9fe068c036
Wicharit-Naay <roycamleu.80@gmail.com>
2025-06-18 20:26:39 +0700

    Initial commit for businesscomputer

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', () => {
+        button.style.transform = 'scale(0.95)';
+        setTimeout(() => {
+            button.style.transform = 'scale(1)';
+        }, 100);
+    });
+});
